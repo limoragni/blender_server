0b846f347ccb904655a5d99a531996fbe3052440
limoragni <guidovillaverde@gmail.com>
2013-12-06 17:45:53 -0300

    Cadena de colas, lograda

--- api/tasks.py
+++ api/tasks.py
@@ -9,28 +9,33 @@ import blender_server.config.environment as env
 import sys
 from django.conf import settings
 
+import logging
+logger = logging.getLogger(__name__)
+
 @app.task
 def sendToBlender(data):
 	try:
-		# files = getFiles(data['media_data'], data['media_url'], data['code'])
-		# #override url de imagenes
-		# data['media_url'] = files
-		# js = json.dumps(data)
-		# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		# s.connect(('127.0.0.1', 13373))
-		# s.send(js)
-		# response = json.loads(s.recv(1024))
-		# s.close()
-		
+		files = getFiles(data['media_data'], data['media_url'], data['code'])
+		#override url de imagenes
+		data['media_url'] = files
+		js = json.dumps(data)
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(('127.0.0.1', 13373))
+		s.send(js)
+		response = json.loads(s.recv(1024))
+		s.close()
+				
 		client = requests.session()
-		client.get(env.RENDER_SUCCESS_URL)  # sets cookie
-		csrftoken = client.cookies['csrftoken']
+		x = client.get(env.RENDER_SUCCESS_URL)  # sets cookie
+		#x = requests.get(env.RENDER_SUCCESS_URL)  # sets cookie
+		csrftoken = x.text
+		
 		#response["response"]
-		data = dict(url='lslslsls' , csrfmiddlewaretoken=csrftoken)
-
-		headers = {'content-type': 'application/json'}
-		requests.post(env.RENDER_SUCCESS_URL, data=data, headers=headers)
-		return "OK"
+		send = dict(code=data['code'], url= response['url'] , csrfmiddlewaretoken=csrftoken)
+		
+		r = client.post(env.RENDER_SUCCESS_URL, data=send, headers=dict(Referer=env.RENDER_SUCCESS_URL))
+		logger.error(r.text)
+		#return g.text
 	except Exception as e:
 		err = {'TASK-error': e}
 		return err
--- blender_server/settings.py
+++ blender_server/settings.py
@@ -95,3 +95,36 @@ REST_FRAMEWORK = {
 }
 
 CELERY_RESULT_BACKEND= 'djcelery.backends.database:DatabaseBackend'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'api.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['file'],
+            'propagate': True,
+            'level':'DEBUG',
+        },
+        'api': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+        },
+    }
+}
