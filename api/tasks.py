fd9f314a9308e7aeea16e3df59479ba692d7dae3
limoragni <guidovillaverde@gmail.com>
2014-01-09 11:30:38 -0300

    Texts working

--- api/tasks.py
+++ api/tasks.py
@@ -1,68 +1,88 @@
-from blender_server.celery import app
-
 import json
+import pickle
 import socket
 import requests
 import os
 import blender_server.config.environment as env
 import sys
 import time
-from django.conf import settings
+import redis
 
+from blender_server.celery import app
+from django.conf import settings
 from subprocess import Popen, PIPE
 
 import logging
 logger = logging.getLogger(__name__)
 
 @app.task
-def sendToBlender(data):
+def sendToBlender(data, task_id=None):
 	try:
-		
 		files = getFiles(data['media_data'], data['media_url'], data['code'])
 		#override url de imagenes
 		data['media_url'] = files
 		data['image_number'] = env.TEMPLATES[data["template"]]["imgs"]
-		js = json.dumps(data)
+		js_data = pickle.dumps(data)
+		saveData(js_data)
 		blend = openBlender(data["template"])
-		time.sleep(2)
-		response = connect(js, blend)
-		blend.terminate()
+		#time.sleep(2)
+		#response = connect(js, blend)
+		#blend.terminate()
+		
+		response = getResponse()
 		vids = convert(response["path"], data['render_type'], response["url"])
 		onRenderSuccess(data, vids)
-		
+	
 	except Exception as e:
-		err = {'TASK-error': e}
-		logger.error(err)
-		return err
+		name = sys.exc_info()[1]
+		fileName = os.path.basename(sys.exc_info()[2].tb_frame.f_code.co_filename)
+		line = sys.exc_info()[2].tb_lineno
+		err = "Error: " + str(e) + " on " + fileName + " line " + str(line)
+		logger.error(err) 
+		# import traceback, os.path
+		# top = traceback.extract_stack()[-1]
+		# err = str(e) + ', '.join([type(e).__name__, os.path.basename(top[0]), "line " + str(top[1])])
+		# logger.error(err)
+		# # err = {'TASK-error': str(e)}
+		# # logger.error(err)
+		# return err
 
 def openBlender(template):
 	logger.error(env.TEMPLATES[template]["path"])
-
 	blend_args = [env.BLENDER_EXEC, "-b", env.TEMPLATES[template]["path"], "-P", env.BLENDER_SCRIPT]
 	blend = Popen(blend_args)
+	blend.communicate()
 	return blend
 	#stdout, stderr = blend.communicate()
 
-def connect(json_send, blend):
-	logger.error("ENTER CONNECTION")
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('127.0.0.1', 13373))
-	logger.error("CONNECT SUCCESS")
-	logger.error(json_send)
-	s.send(json_send)
-	logger.error("PASS SEND")
-	#stdout, stderr = blend.communicate()
-	response = json.loads(s.recv(1024))
-	logger.error("PASS RESPONSE")
-	logger.error(json.dumps(response))
-	s.close()
-	return response		
+def saveData(data):
+	red = redis.StrictRedis(host='localhost', port=6379, db=0)
+	red.set("current_task", data)
+
+def getResponse():
+	red = redis.StrictRedis(host='localhost', port=6379, db=0)
+	data = red.get("current_task_response")
+	return json.loads(data)
+
+# def connect(json_send, blend):
+# 	logger.error("ENTER CONNECTION")
+# 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 	s.connect(('127.0.0.1', 13373))
+# 	logger.error("CONNECT SUCCESS")
+# 	logger.error(json_send)
+# 	s.send(json_send)
+# 	logger.error("PASS SEND")
+# 	#stdout, stderr = blend.communicate()
+# 	response = json.loads(s.recv(1024))
+# 	logger.error("PASS RESPONSE")
+# 	logger.error(json.dumps(response))
+# 	s.close()
+# 	return response		
 
 def onRenderSuccess(data, vids):
 	client = requests.session()
 	x = client.get(env.RENDER_SUCCESS_URL)  # sets cookie
 	csrftoken = x.text
-	
 	
 	send = dict(code=data['code'], render_type=data["render_type"], urls = json.dumps(vids) , csrfmiddlewaretoken=csrftoken)
 	r = client.post(env.RENDER_SUCCESS_URL, data=send, headers=dict(Referer=env.RENDER_SUCCESS_URL))	
@@ -73,6 +93,7 @@ def convert(path, render_type, response_url):
 	else:
 		quality = 'low'
 	##TODO:MOVER A OTRA TAREA!!
+	
 	# ffmpeg -i /path/ -y  -vcodec libvpx -f webm -preset veryslow -qp 0 -strict experimental path/out.webm
 	#ffmpeg -i path/in -y -bt 1500k -vcodec libtheora -preset veryslow -qp 0 path/out.ogv
 	#ffmpeg -i path/in -y -c:v  libx264 -preset veryslow -qp 0 -strict experimental path/out.mp4
--- blender/scripts/jsonServer.py
+++ blender/scripts/jsonServer.py
@@ -1,8 +1,10 @@
 import socketserver
+import pickle
 import json
 import bpy
 import os
 import sys
+
 
 blend_dir = os.path.dirname(os.path.abspath(__file__))
 if blend_dir not in sys.path:
@@ -10,63 +12,58 @@ if blend_dir not in sys.path:
 
 import config as conf
 
-class Renderer():
-    
-    renderData = {}
-    
-    def render(self, data):
-        self.processJSON(data)
-        self.setTemplate(self.renderData['template'], self.renderData['render_type'])
-        #imgs_source = ["ozymandias01.jpg.001", "mononoke.jpg", "pacific-rim.jpg"]S
-        imgs_remote = self.renderData['media_data']
-        imgs_directory = self.renderData["media_url"]
-        
-        for i,v in enumerate(imgs_remote):
-            bpy.data.images["imagen0" + str(i + 1)].filepath = imgs_directory + "/" + v
+sys.path.append('/opt/python3.3/lib/python3.3/site-packages/') 
+import redis
 
-        bpy.data.scenes["Scene"].render.filepath = os.path.join(conf.RENDER_PATH, self.renderData["code"] + "_" + self.renderData["render_type"] + '#')
-        bpy.ops.render.render(animation=True);
-        response = {'path': self.getRenderFilePath(), 'url': self.getRenderURL() }
-        return response
+def execAll():
+    data = getData()
+    setImages(data)
+    setText(data)
+    response = renderVideo(data)
+    saveResponse(response)
 
-    def processJSON(self, data):
-        self.renderData = data
+def getData():
+    red = redis.StrictRedis(host='localhost', port=6379, db=0)
+    data = red.get("current_task")
+    red.set('test', data)
+    json_data = pickle.loads(data)
+    return json_data
 
-    def getFilename(self):
-        fs = bpy.data.scenes["Scene"].frame_start
-        fe = bpy.data.scenes["Scene"].frame_end
-        c = self.renderData["code"] + "_" + self.renderData["render_type"]
-        return c + str(fs) + '-' + str(fe) + conf.CONTAINER
+def setImages(data):
+    imgs_remote = data['media_data']
+    imgs_directory = data["media_url"]
+    for i,v in enumerate(imgs_remote):
+        bpy.data.images["imagen0" + str(i + 1)].filepath = imgs_directory + "/" + v
 
-    def getRenderURL(self):
-        return os.path.join(conf.RENDER_URL, self.getFilename())
+def setText(data):
+    for i,v in enumerate(data['texts']):
+        bpy.context.scene.objects.active = bpy.data.objects["Text.00" + str(i + 1)]
+        bpy.ops.object.editmode_toggle()
+        bpy.ops.font.delete()
+        bpy.ops.font.text_insert(text=str(v))
+        bpy.ops.object.editmode_toggle()
 
-    def getRenderFilePath(self):
-        return os.path.join(conf.RENDER_PATH, self.getFilename())
-    
-    def setTemplate(self, name, render_type):
-        #bpy.ops.wm.open_mainfile(filepath= '/home/limoragni/Dev/djangoapps/blender_server/blender/templates/template-hand/testing02-03_hand.blend')
-        if render_type == "FINAL":
-            quality = 60
-        else:
-            quality = 40
-        bpy.data.scenes["Scene"].render.resolution_percentage = quality
+def renderVideo(data):
+    bpy.data.scenes["Scene"].render.filepath = os.path.join(conf.RENDER_PATH, data["code"] + "_" + data["render_type"] + '#')
+    bpy.ops.render.render(animation=True);
+    response = {'path': getRenderFilePath(data), 'url': getRenderURL(data) }
+    return response
 
-class MyTCPServer(socketserver.ThreadingTCPServer):
-    allow_reuse_address = True
+def saveResponse(response):
+    red = redis.StrictRedis(host='localhost', port=6379, db=0)
+    data = red.set("current_task_response", json.dumps(response))
+    bpy.ops.wm.quit_blender()
 
-class MyTCPServerHandler(socketserver.BaseRequestHandler):
-    def handle(self):
-        try:
-            data = self.request.recv(1024).decode('UTF-8').strip()
-            loaded_data = json.loads(data)
-            r = Renderer()
-            response = r.render(loaded_data)
-            self.request.sendall(bytes(json.dumps(response), 'UTF-8'))
-            bpy.ops.wm.quit_blender()
-        except Exception as e:
-            self.request.sendall(bytes(json.dumps({'BLEND_SERVER_ERROR':"ERROR"}), 'UTF-8'))
-            
+def getFilename(data):
+    fs = bpy.data.scenes["Scene"].frame_start
+    fe = bpy.data.scenes["Scene"].frame_end
+    c = data["code"] + "_" + data["render_type"]
+    return c + str(fs) + '-' + str(fe) + conf.CONTAINER
 
-server = MyTCPServer(('127.0.0.1', 13373), MyTCPServerHandler)
-server.serve_forever()
+def getRenderURL(data):
+    return os.path.join(conf.RENDER_URL, getFilename(data))
+
+def getRenderFilePath(data):
+    return os.path.join(conf.RENDER_PATH, getFilename(data))
+
+execAll()
